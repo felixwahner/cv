88a05ec845c45ca3cb3296c64349da072d5fd65d
Felix Wahner <felix@o315.de>
2024-08-10 23:49:43 +0200

    Add env file for base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
+const BASE_URL = process.env.BASE_URL || ''
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: `${BASE_URL}`,
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
